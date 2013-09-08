acf0685f4257648eb7e0d5562e50545a7f534777
lxdiyun <lxdiyun@gmail.com>
2013-09-08 17:33:00 +0800

    daemon added

--- sip2_proxy_service.py
+++ sip2_proxy_service.py
@@ -1,21 +1,7 @@
 #!/usr/bin/env python2
-from daemon import runner
 from sip2_proxy import start_sip2_proxy_server
+import daemon
 
 
-class App(object):
-    def __init__(self):
-        self.stdin_path = '/dev/null'
-        self.stdout_path = '/home/adli/sip2_proxy/log/stdout'
-        self.stderr_path = '/home/adli/sip2_proxy/log/stderr'
-        self.pidfile_path = '/var/run/sip2_proxy/sip2_proxy.pid'
-        self.pidfile_timeout = 5
-
-    def run(self):
-        start_sip2_proxy_server()
-
-
-app = App()
-
-daemon_runner = runner.DaemonRunner(app)
-daemon_runner.do_action()
+with daemon.DaemonContext():
+    start_sip2_proxy_server()
