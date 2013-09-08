a0aa110925896a9f8bf7b18e19d6868160c86369
lxdiyun <lxdiyun@gmail.com>
2013-09-08 17:53:50 +0800

    typo error

--- sip2_proxy_service.py
+++ sip2_proxy_service.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python2
 from sip2_proxy import start_sip2_proxy_server
 import daemon
+import sys
 
 
-with daemon.DaemonContext():
-    start_sip2_proxy_server()
+if __name__ == "__main__":
+    with daemon.DaemonContext():
+        start_sip2_proxy_server()
